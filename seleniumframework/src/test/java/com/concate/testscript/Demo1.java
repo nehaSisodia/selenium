68c686e5f2ad00e4ca39eebbca72b7a2ce793ddf
neha <sky32neha@gmail.com>
2021-03-15 13:00:13 +0530

    1st commite

---
+++ seleniumframework/src/test/java/com/concate/testscript/Demo1.java
@@ -0,0 +1,13 @@
+package com.concate.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void createCustomer()
+	{
+		Reporter.log("createcustomer",true);
+	}
+}
